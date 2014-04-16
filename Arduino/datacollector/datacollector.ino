c7773c794fbc120a83a9b2ba53260b4d915bece1
Mathieu Poignet <mathieu.poignet@gmail.com>
2014-04-16 04:41:57 +0200

    trying to do some oop

--- Arduino/datacollector/datacollector.ino
+++ Arduino/datacollector/datacollector.ino
@@ -1,194 +1,199 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include "EmonLib.h"                   
 #include <SPI.h>
+#include <EmonLib.h> 
 #include <Ethernet.h>
-#include <MemoryFree.h>
-
-EnergyMonitor emon1;                   // Create an instance
-
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = { 
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192,168,2,77);
-
-// Initialize the Ethernet server library
-// with the IP address and port you want to use 
-// (port 80 is default for HTTP):
-EthernetServer server(88);
-//------------------------------------
 
 // Data wire is plugged into port 2 on the Arduino
-#define ONE_WIRE_BUS 2
+#define NB_TERMO 5
+
 #define TEMPERATURE_PRECISION 12
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+class Thermometers {
+    int precision, pin;
+    float lastTemp;
+    String name;
+    OneWire *oneWire;
+    DallasTemperature *manager;
+  public:
+    Thermometers ();
+    Thermometers (String,int,int);
+    Thermometers (String,int);
+    void requestTemp ();
+    void retrieveTemp ();
+    String getDescription ();
+    String getName ();
+    float getTemperature ();
+    int getPin ();
+    void start();
+    void init(String,int,int);
+};
 
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
-
-int numberOfDevices; // Number of temperature devices found
-
-int nbConnexions;
-
-DeviceAddress tempDeviceAddress; // We'll use this variable to store a found device address
-
-void setup(void)
-{
-   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  
-  emon1.current(1, 66.6);             // Current: input pin, calibration.
-  
-  nbConnexions = 0;
-
-  // start the Ethernet connection and the server:
-  Ethernet.begin(mac, ip);
-  server.begin();
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP());
-  
-  // start serial port
-  Serial.println("Dallas Temperature IC Control Library Demo");
-
-  // Start up the library
-  sensors.begin();
-  
-  // Grab a count of devices on the wire
-  numberOfDevices = sensors.getDeviceCount();
-  
-  // locate devices on the bus
-  Serial.print("Locating devices...");
-  Serial.print("Found ");
-  Serial.print(numberOfDevices, DEC);
-  Serial.println(" devices.");
-
-  // report parasite power requirements
-  Serial.print("Parasite power is: "); 
-  if (sensors.isParasitePowerMode()) Serial.println("ON");
-  else Serial.println("OFF");
-  
-  // Loop through each device, print out address
-  for(int i=0;i<numberOfDevices; i++)
-  {
-    // Search the wire for address
-    if(sensors.getAddress(tempDeviceAddress, i))
-	{
-		Serial.print("Found device ");
-		Serial.print(i, DEC);
-		Serial.print(" with address: ");
-		printAddress(tempDeviceAddress);
-		Serial.println();
-		
-		Serial.print("Setting resolution to ");
-		Serial.println(TEMPERATURE_PRECISION,DEC);
-		
-		// set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
-		sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
-		
-		 Serial.print("Resolution actually set to: ");
-		Serial.print(sensors.getResolution(tempDeviceAddress), DEC); 
-		Serial.println();
-	}else{
-		Serial.print("Found ghost device at ");
-		Serial.print(i, DEC);
-		Serial.print(" but could not detect address. Check power and cabling");
-	}
-  }
+Thermometers::Thermometers () {
 
 }
 
+Thermometers::Thermometers (String name_, int pin_) {
+  init(name_, pin_, 12);
+}
+
+Thermometers::Thermometers (String name_, int pin_, int precision_) {
+  init(name_, pin_, precision_);
+}
+
+float Thermometers::getTemperature () {
+  return lastTemp;
+}
+
+int Thermometers::getPin () {
+  return pin;
+}
+
+String Thermometers::getName () {
+  return name;
+}
+
+void Thermometers::init (String name_, int pin_, int precision_) {
+  name = name_;
+  pin = pin_;
+  precision = precision_;
+  oneWire = new OneWire(pin);
+  manager = new DallasTemperature(oneWire);
+  manager->begin();
+  manager->setResolution(precision);
+}
+
+
+void Thermometers::requestTemp() {
+  manager->requestTemperatures();
+}
+
+void Thermometers::retrieveTemp () {
+  DeviceAddress tempDeviceAddress; 
+  if(manager->getAddress(tempDeviceAddress, 0)){
+    lastTemp = manager->getTempC(tempDeviceAddress);
+  } 
+  
+}
+
+String Thermometers::getDescription() {
+  return "Thermometer " + name + "\n" 
+    + "  pin: "+pin+"\n"
+    + "  resolution: "+manager->getResolution()+"\n"
+    + "  parasite mode: "+manager->isParasitePowerMode()+"\n";
+}
+
+// Ethernet server init
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192,168,2,77);
+EthernetServer server(88);
+
+Thermometers *t0;
+Thermometers *t1;
+Thermometers *t2;
+Thermometers *t3;
+Thermometers *t4;
+EnergyMonitor emon1;
+
+void setup(void)
+{
+  Serial.println("BEGIN");
+  Serial.begin(9600); 
+//  for(uint16_t i = 0; i < NB_TERMO; i++) {
+//    thermometers[i] = (Thermometers *) malloc(sizeof(Thermometers));
+//  } 
+  
+  t0 = new Thermometers("t0", 2, 12);
+  t1 = new Thermometers("t1", 4, 12);
+  t2 = new Thermometers("t1", 5, 12);
+  t3 = new Thermometers("t1", 6, 12);
+  t4 = new Thermometers("t1", 7, 12);
+  //thermometers[2]->init("t2", 5, 12);
+  //thermometers[3]->init("t3", 6, 12);
+  //thermometers[4]->init("t4", 7, 12);
+
+  
+//  for(int i=0;i<NB_TERMO; i++){
+//    Serial.println(thermometers[i]->getDescription());
+//  }
+
+  t0->getDescription();
+  t1->getDescription();
+  t2->getDescription();
+  t3->getDescription();
+  t4->getDescription();
+  
+  emon1.current(1, 66.6);             // Current: input pin, calibration.
+  
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+}
 
 void loop(void)
 { 
-  // call sensors.requestTemperatures() to issue a global temperature 
-  // request to all devices on the bus
-  Serial.print("Requesting temperatures...");
-  sensors.requestTemperatures(); // Send the command to get temperatures
+  Thermometers thermometers[NB_TERMO] = {*t0, *t1, *t2, *t3, *t4};
+  Serial.println("Requesting temperatures...");
+//  for(int i=0;i<NB_TERMO; i++){
+//    thermometers[i]->requestTemp(); 
+//  }
+  for(int i=0;i<NB_TERMO; i++){
+    thermometers[i].requestTemp(); 
+  }
+  delay(1000);
   Serial.println("DONE");
+  
   Serial.print("Requesting current...");
   double Irms = emon1.calcIrms(1480);  // Calculate Irms only
   Serial.println("DONE");
-  //String IrmsString = String(Irms);
-  Serial.println(Irms);
   
-  // Loop through each device, print out temperature data
-  for(int i=0;i<numberOfDevices; i++)
-  {
-    // Search the wire for address
-    if(sensors.getAddress(tempDeviceAddress, i))
-	{
-		// Output the device ID
-		Serial.print("Temperature for device: ");
-		Serial.println(i,DEC);
-		
-		// It responds almost immediately. Let's print out the data
-		printTemperature(tempDeviceAddress); // Use a simple function to print out the data
-	} 
-	//else ghost device! Check your power requirements and cabling
-	
+  for(int i=0;i<NB_TERMO; i++){
+    thermometers[i].retrieveTemp();
   }
   
-  delay(100);
-  Serial.println(":");
-  Serial.println(":");
-  Serial.println(":");
+  for(int i=0;i<NB_TERMO; i++){
+    Serial.print("Temperature for device ");
+    Serial.print(thermometers[i].getPin());
+    Serial.print(": ");
+    Serial.println(thermometers[i].getTemperature());
+  }
   
-  
-  //-----------------------
-  
+
+
   // listen for incoming clients
   EthernetClient client = server.available();
   if (client) {
-    nbConnexions++;
-    Serial.println("new client");
+
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
         Serial.write(c);
+        
         // if you've gotten to the end of the line (received a newline
         // character) and the line is blank, the http request has ended,
         // so you can send a reply
         if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/json");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-	  //client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-
+          
+          printHeader(client);
           client.print("{");
           client.print("\"date\":0,"); 
           client.print("\"c\":"); 
           client.print(Irms);
-          client.print(",");
-          
-          int mem = freeMemory();
-          client.print("\"m\":"); 
-          client.print(mem);
-          client.print(",");
-          
-          for(int i=0;i<numberOfDevices; i++)
-          {
-            // Search the wire for address
-            if(sensors.getAddress(tempDeviceAddress, i))
-        	{
-                        client.print("\"");
-        		client.print(i,DEC);
-                        client.print("\":");
-        		float tempC = sensors.getTempC(tempDeviceAddress);
-                        client.print(tempC);
-                        if(i<numberOfDevices-1){
-                          client.print(",");
-                        }
-        	} 
-        	//else ghost device! Check your power requirements and cabling
-          }
-          
+          client.print(",");  
+   
+          for(int i=0;i<NB_TERMO; i++){
+            client.print("\"");
+            client.print(i,DEC);
+            client.print("\":");
+            float tempC = thermometers[i].getTemperature();
+            client.print(tempC);
+            if(i<NB_TERMO-1){
+              client.print(",");
+            }
+          }       
+
           client.print("}");
           break;
         }
@@ -206,21 +211,21 @@ void loop(void)
     delay(1);
     // close the connection:
     client.stop();
-    Serial.println("client disonnected");
-    if(nbConnexions > 240){
-      software_Reset();
-    }
+    //Serial.println("client disonnected");
+//    if(nbConnexions > 240){
+//      software_Reset();
+//    }
   }
-  
-  
+}
 
-  //-----------------------------------------
-  
+void printHeader(EthernetClient client){
+// send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/json");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println();  
 }
 
 
-void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
-  {
-    asm volatile ("  jmp 0");  
-  } 
+
 
--- Arduino/datacollector/printTemperature.ino
+++ Arduino/datacollector/printTemperature.ino
@@ -1,10 +1,10 @@
 // function to print the temperature for a device
-void printTemperature(DeviceAddress deviceAddress)
-{
-  // method 2 - faster
-  float tempC = sensors.getTempC(deviceAddress);
-  Serial.print("Temp C: ");
-  Serial.print(tempC);
-  Serial.print(" Temp F: ");
-  Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
-}
+//void printTemperature(DeviceAddress deviceAddress)
+//{
+//  // method 2 - faster
+//  float tempC = sensors.getTempC(deviceAddress);
+//  Serial.print("Temp C: ");
+//  Serial.print(tempC);
+//  Serial.print(" Temp F: ");
+//  Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
+//}
