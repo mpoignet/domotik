4e07afb943c9530808478a2f6f4795cd952c19cb
Mathieu Poignet <mathieu.poignet@gmail.com>
2014-05-03 17:12:02 +0200

    improving the datafetcher, pretty logs and all

--- dataFetcher.py
+++ dataFetcher.py
@@ -1,30 +1,55 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 import urllib2
 import json
 import datetime
+import time
 import MySQLdb
+import sys
+import pprint
 
-# Database connection
-db = MySQLdb.connect(host="localhost", user="oasis", passwd="oasis", db="oasis") 				  
-cur = db.cursor() 
- 
-# Fetching data from arduino
-jsonContent = urllib2.urlopen("http://192.168.2.77:88/").read()
-measures = json.loads(jsonContent)   
-measures["date"] = datetime.datetime.now()  
+def log(string):
+	print(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')) +' : '+ string)
 
-print(measures["m"])
+serverAdress = "http://192.168.2.79/"
 
-#Converting data types to please the db
-measures["date"] = measures["date"].strftime('%Y-%m-%d %H:%M:%S')
+while(True):
+	# Database connection
+	db = MySQLdb.connect(host="localhost", user="oasis", passwd="oasis", db="oasis") 				  
+	cur = db.cursor() 
+	 
+	# Fetching data from arduino
+	jsonContent = False
+	try:
+		log('Contacting server at '+serverAdress)
+		jsonContent = urllib2.urlopen(serverAdress).read()
+	except:
+		log('ERROR: Server is unreachable')
+		time.sleep(15)
 
-# Inserting data into the database
-try:
-    cur.execute("INSERT INTO data(date, t0, t1, t2, t3, t4, c0) VALUES (%s,%s,%s,%s,%s,%s,%s)",(measures["date"],measures["0"],measures["1"],measures["2"],measures["3"],measures["4"],measures["c"]))
-    db.commit()
-except MySQLdb.Error, e:
-    print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-    db.rollback()
-  
-db.close()					
+	if(jsonContent):
+		measures = json.loads(jsonContent)   
+		measures["date"] = datetime.datetime.now()  
+		goodReading = True;
+		log('Measures: '+str(measures))
+
+		#Checking the data 
+		for p in measures.keys():
+			if(measures[p]==85):
+				goodReading=False
+
+		if(goodReading):
+			#Converting data types to please the db
+			measures["date"] = measures["date"].strftime('%Y-%m-%d %H:%M:%S')
+
+			# Inserting data into the database
+			try:
+			    cur.execute("INSERT INTO data(date, t0, t1, t2, t3, t4, c0) VALUES (%s,%s,%s,%s,%s,%s,%s)",(measures["date"],measures["0"],measures["1"],measures["2"],measures["3"],measures["4"],measures["c"]))
+			    db.commit()
+			except MySQLdb.Error, e:
+			    log("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+			    db.rollback()
+			  
+			db.close()	
+			time.sleep(15)				
+
 
