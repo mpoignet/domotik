00ba1a2dfcf22e5db25a03c298769764e9f88002
Mathieu Poignet <mathieu.poignet@gmail.com>
2014-05-11 18:31:52 +0200

    Better error handling for the datafetcher

--- dataFetcher.py
+++ dataFetcher.py
@@ -6,9 +6,14 @@ import time
 import MySQLdb
 import sys
 import pprint
+from timeout import timeout
 
 def log(string):
 	print(str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')) +' : '+ string)
+
+@timeout(10)
+def getData(serverAdress):
+	return urllib2.urlopen(serverAdress).read()
 
 serverAdress = "http://192.168.2.79/"
 
@@ -21,21 +26,25 @@ while(True):
 	jsonContent = False
 	try:
 		log('Contacting server at '+serverAdress)
-		jsonContent = urllib2.urlopen(serverAdress).read()
+		jsonContent = getData(serverAdress)
 	except:
 		log('ERROR: Server is unreachable')
 		time.sleep(15)
 
 	if(jsonContent):
-		measures = json.loads(jsonContent)   
-		measures["date"] = datetime.datetime.now()  
-		goodReading = True;
-		log('Measures: '+str(measures))
+		try:
+			measures = json.loads(jsonContent)   
+			measures["date"] = datetime.datetime.now()  
+			goodReading = True;
+			log('Measures: '+str(measures))
 
-		#Checking the data 
-		for p in measures.keys():
-			if(measures[p]==85):
-				goodReading=False
+			#Checking the data 
+			for p in measures.keys():
+				if(measures[p]==85):
+					goodReading=False
+		except:
+			log('ERROR: Parsing exception')
+			break;
 
 		if(goodReading):
 			#Converting data types to please the db
@@ -48,6 +57,9 @@ while(True):
 			except MySQLdb.Error, e:
 			    log("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
 			    db.rollback()
+			except:
+				log("ERROR: Problem inserting in the database")
+				db.rollback()
 			  
 			db.close()	
 			time.sleep(15)				
