fa61f9d6103da489a341e09a305bddbe4b48395a
Mathieu Poignet <mathieu.poignet@gmail.com>
2014-05-20 04:41:25 +0200

    adding the date arguments to get temperatures

--- Website/temperatures/models.py
+++ Website/temperatures/models.py
@@ -2,22 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-# class Data(models.Model):
-#     date = models.DateTimeField(blank=True, null=True)
-#     t0 = models.FloatField(blank=True, null=True)
-#     t1 = models.FloatField(blank=True, null=True)
-#     t2 = models.FloatField(blank=True, null=True)
-#     t3 = models.FloatField(blank=True, null=True)
-#     t4 = models.FloatField(blank=True, null=True)
-#     c0 = models.FloatField(blank=True, null=True)
-#     def __unicode__(self):  # Python 3: def __str__(self):
-#         return str(self.date.strftime('%Y-%m-%d %H:%M:%S'))
-
-#     class Meta:
-#         managed = False
-#         db_table = 'data'
-
-
 class Device(models.Model):
     def __unicode__(self):
         if(self.name):  
--- Website/temperatures/views.py
+++ Website/temperatures/views.py
@@ -4,11 +4,14 @@ from django.http import HttpResponse
 from temperatures.models import Device, Record
 from django.core import serializers
 import json
+import datetime
 
 # Create your views here.
 
 def list(request):
     sensorList = Device.objects.all().values()
+    startDate = request.GET.get('startDate', str((datetime.datetime.now()-datetime.timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')))
+    endDate = request.GET.get('endDate', str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
     sensors = []
     for sensor in sensorList:
@@ -19,10 +22,13 @@ def list(request):
             s['name'] = sensor['name']
 
         s['values'] = []
-        records = Record.objects.filter(device_id=sensor['id']).values()
+        records = Record.objects.filter(device_id=sensor['id']).filter(date__gte=startDate).filter(date__lte=endDate)
+        #nbRecord = 25000
+        #if(records.count()>=nbRecord):
+        #    records = records.order_by('date')[records.count()-nbRecord:records.count()].values()
         for record in records:
-            dateString = str(record['date'].strftime('%Y-%m-%d %H:%M:%S'))
-            s['values'].append({'date': dateString, 'temperature': record['measure']})
-        sensors.append(s)
+            dateString = str(record.date.strftime('%Y-%m-%d %H:%M:%S'))
+            s['values'].append({'date': dateString, 'temperature': record.measure})
+        sensors.append(s)   
 
     return HttpResponse(json.dumps({'sensors': sensors}), content_type="application/json")
