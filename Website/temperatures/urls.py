96b23a9d39fd819eba0996006072ea822b795b39
Mathieu Poignet <mathieu.poignet@gmail.com>
2014-05-21 21:00:54 +0200

    adding possibility to request device by device

--- Website/temperatures/urls.py
+++ Website/temperatures/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from temperatures import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.list, name='list')
+    url(r'^$', views.getData, name='getData'),
+    url(r'list', views.getList, name='getList'),
 )
--- Website/temperatures/views.py
+++ Website/temperatures/views.py
@@ -8,10 +8,28 @@ import datetime
 
 # Create your views here.
 
-def list(request):
+def getList(request):
     sensorList = Device.objects.all().values()
-    startDate = request.GET.get('startDate', str((datetime.datetime.now()-datetime.timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')))
-    endDate = request.GET.get('endDate', str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    sensors = []
+    for sensor in sensorList:
+        s={}
+        if(not sensor['name']):
+            s['name'] = str(sensor['address'])
+        else:
+            s['name'] = sensor['name']
+        s['id'] = sensor['id']
+        sensors.append(s)   
+    return HttpResponse(json.dumps({'sensors': sensors}), content_type="application/json")
+
+
+def getData(request):
+    sensorList = request.GET.getlist('sensors', [])
+    if(sensorList):
+        sensorList = Device.objects.filter(pk__in=sensorList).values()
+    else:
+        sensorList = Device.objects.all().values()
+    startDate = request.GET.get('startDate', '')
+    endDate = request.GET.get('endDate', '')
 
     sensors = []
     for sensor in sensorList:
@@ -20,16 +38,22 @@ def list(request):
             s['name'] = str(sensor['address'])
         else:
             s['name'] = sensor['name']
-
         s['values'] = []
-        records = Record.objects.filter(device_id=sensor['id']).filter(date__gte=startDate).filter(date__lte=endDate)
+        if(startDate and endDate):
+            records = Record.objects.filter(device_id=sensor['id']).filter(date__gte=startDate).filter(date__lte=endDate)
+        elif(startDate):
+            records = Record.objects.filter(device_id=sensor['id']).filter(date__gte=startDate)
+        else:
+            records = Record.objects.filter(device_id=sensor['id'])
+            records = records.order_by('-date')[0:250]
+        print startDate
+        print endDate
         if(len(records) > 0):
-            #nbRecord = 25000
-            #if(records.count()>=nbRecord):
-            #    records = records.order_by('date')[records.count()-nbRecord:records.count()].values()
             for record in records:
                 dateString = str(record.date.strftime('%Y-%m-%d %H:%M:%S'))
                 s['values'].append({'date': dateString, 'temperature': record.measure})
             sensors.append(s)   
 
     return HttpResponse(json.dumps({'sensors': sensors}), content_type="application/json")
+
+
