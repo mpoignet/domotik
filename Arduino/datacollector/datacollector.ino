49e565979e63108edf41a446e1d8f80a867dd9e9
Mathieu Poignet <mathieu.poignet@gmail.com>
2014-06-13 15:37:34 +0200

    The datacollector handles 2 one wire bus

--- Arduino/datacollector/datacollector.ino
+++ Arduino/datacollector/datacollector.ino
@@ -3,43 +3,58 @@
 #include "EmonLib.h"                   // Include Emon Library
 #include <SPI.h>
 #include <Ethernet.h>
+#include <string.h>
 
 EnergyMonitor emon1;                   // Create an instance
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
+/*******************************************************************
+*                 Server Configuration
+********************************************************************/
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192,168,2,79);
-
-// Initialize the Ethernet server library
-// with the IP address and port you want to use 
-// (port 80 is default for HTTP):
+IPAddress ip(192,168,2,68);
 EthernetServer server(80);
-//------------------------------------
 
-// Data wire is plugged into port 2 on the Arduino
-#define ONE_WIRE_BUS 7
+
+#define bufferMax 128
+int bufferSize;
+char buffer[bufferMax];
+
+//-- Commands and parameters (sent by browser) --
+char cmd[15];    
+char param1[15];
+
+/*******************************************************************
+*                 Thermometers Configuration
+********************************************************************/
+#define ONE_WIRE_BUS_1 6
+#define ONE_WIRE_BUS_2 7
 #define TEMPERATURE_PRECISION 12
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire1(ONE_WIRE_BUS_1);
+OneWire oneWire2(ONE_WIRE_BUS_2);
 
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
+DallasTemperature sensors1(&oneWire1);
+DallasTemperature sensors2(&oneWire2);
 
-int numberOfDevices; // Number of temperature devices found
-
-int resetPin = 12;
+int numberOfDevices1; // Number of temperature devices found
+int numberOfDevices2;
 
 DeviceAddress tempDeviceAddress; // We'll use this variable to store a found device address
 
+/*******************************************************************
+*                 Current sensor Configuration
+********************************************************************/
+#define CURRENT_SENSOR_PIN 1
+
+
+/*******************************************************************
+*                    Setup
+********************************************************************/
 void setup(void){
-    digitalWrite(resetPin, HIGH);
-    // Open serial communications and wait for port to open:
     Serial.begin(9600);
 
     // Current: input pin, calibration.
-    emon1.current(1, 66.6);             
+    emon1.current(CURRENT_SENSOR_PIN, 66.6);             
 
     // start the Ethernet connection and the server:
     Ethernet.begin(mac, ip);
@@ -48,9 +63,12 @@ void setup(void){
     Serial.println(Ethernet.localIP());
 
     // start serial port
-    Serial.println("Dallas Temperature IC Control Library Demo");
+    Serial.println("Hostel Automation Datacollector");
 }
 
+/*******************************************************************
+*                    Main Loop
+********************************************************************/
 
 void loop(void){ 
 
@@ -58,27 +76,44 @@ void loop(void){
 *                 Recording temperatures
 ********************************************************************/
 
+      
+     Serial.println("Loop 1");
     // Start up the Thermometers library
-    sensors.begin();
+    sensors1.begin();
+    sensors2.begin();
+    Serial.println("Loop 2");  
 
     // Grab a count of devices on the wire
-    numberOfDevices = sensors.getDeviceCount();
+    numberOfDevices1 = sensors1.getDeviceCount();
+    numberOfDevices2 = sensors2.getDeviceCount();
+    Serial.println("Loop 3");
 
-    // locate devices on the bus
+    // // locate devices on the bus
     Serial.print("Locating devices...");
     Serial.print("Found ");
-    Serial.print(numberOfDevices, DEC);
-    Serial.println(" devices.");
+    Serial.print(numberOfDevices1, DEC);
+    Serial.print(" devices on pin ");
+    Serial.println(ONE_WIRE_BUS_1, DEC);
+    Serial.print("Found ");
+    Serial.print(numberOfDevices2, DEC);
+    Serial.print(" devices on pin ");
+    Serial.println(ONE_WIRE_BUS_2, DEC);
 
-    // report parasite power requirements
-    Serial.print("Parasite power is: "); 
-    if (sensors.isParasitePowerMode()) Serial.println("ON");
+    // // report parasite power requirements
+    Serial.println("Parasite power is: "); 
+    Serial.print("Bus 1: "); 
+    if (sensors1.isParasitePowerMode()) Serial.println("ON");
     else Serial.println("OFF");
+    Serial.print("Bus 2: "); 
+    if (sensors2.isParasitePowerMode()) Serial.println("ON");
+    else Serial.println("OFF");
+    
+    Serial.println("-- BUS 1 --");
 
-    // Loop through each device, print out address
-    for(int i=0;i<numberOfDevices; i++){
+    // // Loop through each device, print out address
+    for(int i=0;i<numberOfDevices1; i++){
         // Search the wire for address
-        if(sensors.getAddress(tempDeviceAddress, i)){
+        if(sensors1.getAddress(tempDeviceAddress, i)){
             Serial.print("Found device ");
             Serial.print(i, DEC);
             Serial.print(" with address: ");
@@ -89,11 +124,38 @@ void loop(void){
             Serial.println(TEMPERATURE_PRECISION,DEC);
 
             // set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
-            sensors.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
+            sensors1.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
 
             Serial.print("Resolution actually set to: ");
-            Serial.print(sensors.getResolution(tempDeviceAddress), DEC); 
+            Serial.println(sensors1.getResolution(tempDeviceAddress), DEC); 
+            
+        }else{
+            Serial.print("Found ghost device at ");
+            Serial.print(i, DEC);
+            Serial.print(" but could not detect address. Check power and cabling");
+        }
+    }
+    
+    Serial.println("-- BUS 2 --");
+
+    // // Loop through each device, print out address
+    for(int i=0;i<numberOfDevices2; i++){
+        // Search the wire for address
+        if(sensors2.getAddress(tempDeviceAddress, i)){
+            Serial.print("Found device ");
+            Serial.print(i, DEC);
+            Serial.print(" with address: ");
+            printAddressToSerial(tempDeviceAddress);
             Serial.println();
+
+            Serial.print("Setting resolution to ");
+            Serial.println(TEMPERATURE_PRECISION,DEC);
+
+            // set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
+            sensors2.setResolution(tempDeviceAddress, TEMPERATURE_PRECISION);
+
+            Serial.print("Resolution actually set to: ");
+            Serial.println(sensors2.getResolution(tempDeviceAddress), DEC); 
         }else{
             Serial.print("Found ghost device at ");
             Serial.print(i, DEC);
@@ -104,93 +166,201 @@ void loop(void){
     // call sensors.requestTemperatures() to issue a global temperature 
     // request to all devices on the bus
     Serial.print("Requesting temperatures...");
-    sensors.requestTemperatures(); // Send the command to get temperatures
+    sensors1.requestTemperatures(); // Send the command to get temperatures
+    sensors2.requestTemperatures(); // Send the command to get temperatures
     Serial.println("DONE");
     Serial.print("Requesting current...");
     double Irms = emon1.calcIrms(1480);  // Calculate Irms only
     Serial.println("DONE");
-    //String IrmsString = String(Irms);
     Serial.println(Irms);
   
     // Loop through each device, print out temperature data
-    for(int i=0;i<numberOfDevices; i++){
+    for(int i=0;i<numberOfDevices1; i++){
         // Search the wire for address
-        if(sensors.getAddress(tempDeviceAddress, i)){
+        if(sensors1.getAddress(tempDeviceAddress, i)){
             // Output the device ID
             Serial.print("Temperature for device ");
             printAddressToSerial(tempDeviceAddress);
             Serial.print(": ");
             // It responds almost immediately. Let's print out the data
-            float tempC = sensors.getTempC(tempDeviceAddress);
+            float tempC = sensors1.getTempC(tempDeviceAddress);
             Serial.println(tempC);
         } 
     }
+    for(int i=0;i<numberOfDevices2; i++){
+        // Search the wire for address
+        if(sensors2.getAddress(tempDeviceAddress, i)){
+            // Output the device ID
+            Serial.print("Temperature for device ");
+            printAddressToSerial(tempDeviceAddress);
+            Serial.print(": ");
+            // It responds almost immediately. Let's print out the data
+            float tempC = sensors2.getTempC(tempDeviceAddress);
+            Serial.println(tempC);
+        } 
+    }
+    Serial.println("Loop 4");
 delay(100);
-Serial.println(":");
-Serial.println(":");
-Serial.println(":");
+// Serial.println(":");
+// Serial.println(":");
+// Serial.println(":");
 
 
 /*******************************************************************
 *                 Publishing temperatures
 ********************************************************************/
 
-    // listen for incoming clients
     EthernetClient client = server.available();
-    if (client) {
-        Serial.println("new client");
-        // an http request ends with a blank line
-        boolean currentLineIsBlank = true;
-        while (client.connected()) {
-            if (client.available()) {
-                char c = client.read();
-                Serial.write(c);
-                // if you've gotten to the end of the line (received a newline
-                // character) and the line is blank, the http request has ended,
-                // so you can send a reply
-                if (c == '\n' && currentLineIsBlank) {
-                    // send a standard http response header
-                    client.println("HTTP/1.1 200 OK");
-                    client.println("Content-Type: text/json");
-                    client.println("Connection: close");  // the connection will be closed after completion of the response
-                    client.println();
-                    client.print("{");
-                    //client.print("\"c\":"); 
-                    //client.print(Irms);
-                    //client.print(",");
+    if (client){
+        Serial.println("Client connexion");
+        WaitForRequest(client);
+        server.println("HTTP/1.1 200 OK");
+        server.println("Content-Type: text/json");
+        server.println("Connection: close");  // the connection will be closed after completion of the response
+        server.println();
+        server.print("{");
+        server.print("\"c"); 
+        server.print(CURRENT_SENSOR_PIN); 
+        server.print("\":"); 
+        server.print(emon1.calcIrms(1480));
+        server.print(",");
 
-                    for(int i=0;i<numberOfDevices; i++){
-                        // Search the wire for address
-                        if(sensors.getAddress(tempDeviceAddress, i)){
-                            client.print("\"");
-                            printAddressToClient(tempDeviceAddress, client);
-                            client.print("\":");
-                            float tempC = sensors.getTempC(tempDeviceAddress);
-                            client.print(tempC);
+        for(int i=0;i<numberOfDevices1; i++){
+            // Search the wire for address
+            if(sensors1.getAddress(tempDeviceAddress, i)){
+                server.print("\"");
+                printAddressToClient(tempDeviceAddress);
+                server.print("\":");
+                float tempC = sensors1.getTempC(tempDeviceAddress);
+                server.print(tempC);
 
-                            if(i<numberOfDevices-1){
-                                client.print(",");
-                            }
-                        } 
-                    }
-                    client.print("}");
-                    break;
-                }
-                if (c == '\n') {
-                    // you're starting a new line
-                    currentLineIsBlank = true;
-                } 
-                else if (c != '\r') {
-                    // you've gotten a character on the current line
-                    currentLineIsBlank = false;
-                }
-            }
+                server.print(",");
+                
+            } 
         }
-        // give the web browser time to receive the data
-        delay(1);
-        // close the connection:
-        client.stop();
-        Serial.println("client disonnected");
+
+        for(int i=0;i<numberOfDevices2; i++){
+            // Search the wire for address
+            if(sensors2.getAddress(tempDeviceAddress, i)){
+                server.print("\"");
+                printAddressToClient(tempDeviceAddress);
+                server.print("\":");
+                float tempC = sensors2.getTempC(tempDeviceAddress);
+                server.print(tempC);
+
+                if(i<numberOfDevices2-1){
+                    server.print(",");
+                }
+            } 
+        }
+
+    server.print("}");
+    delay(1);
+    client.stop();
     }
 }
 
+void WaitForRequest(EthernetClient client) // Sets buffer[] and bufferSize
+{
+  bufferSize = 0;
+ 
+  while (client.connected()) {
+    if (client.available()) {
+      char c = client.read();
+      if (c == '\n')
+        break;
+      else
+        if (bufferSize < bufferMax)
+          buffer[bufferSize++] = c;
+        else
+          break;
+    }
+  }
+  
+  PrintNumber("bufferSize", bufferSize);
+}
+
+void ParseReceivedRequest()
+{
+  Serial.println("in ParseReceivedRequest");
+  Serial.println(buffer);
+  
+  //Received buffer contains "GET /cmd/param1/param2 HTTP/1.1".  Break it up.
+  char* slash1;
+  char* slash2;
+  char* space2;
+  
+  slash1 = strstr(buffer, "/") + 1; // Look for first slash
+  slash2 = strstr(slash1, "/") + 1; // second slash
+  space2 = strstr(slash1, " ") + 1; // space after second slash (in case there is no third slash)
+  if (slash2 > space2) slash2=slash1;
+
+  PrintString("slash1",slash1);
+  PrintString("slash2",slash2);
+  PrintString("space2",space2);
+  
+  // strncpy does not automatically add terminating zero, but strncat does! So start with blank string and concatenate.
+  cmd[0] = 0;
+  param1[0] = 0;
+  strncat(cmd, slash1, slash2-slash1-1);
+  strncat(param1, slash2, space2-slash2-1);
+  
+  PrintString("cmd",cmd);
+  PrintString("param1",param1);
+}
+
+void PerformRequestedCommands()
+{
+     if ( strcmp(cmd,"read") != 0 ) return;
+     Serial.println("perform");
+     int bus = param1[0] - '0';
+     PrintNumber("bus", bus);
+     int numberOfDevices;
+     DallasTemperature *sensors;
+     if(bus == 1){
+         Serial.println("bus 1");
+         numberOfDevices = numberOfDevices1;
+         sensors = &sensors1;
+     }else if(bus == 2){
+         Serial.println("bus 2");
+         numberOfDevices = numberOfDevices2;
+         sensors = &sensors2;
+     }else{
+       return;
+     }
+  // send a standard http response header
+    
+}
+
+
+void printAddressToSerial(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+void printAddressToClient(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) server.print("0");
+    server.print(deviceAddress[i], HEX);
+  }
+}
+
+void PrintString(char* label, char* str)
+{
+  Serial.print(label);
+  Serial.print("=");
+  Serial.println(str);
+}
+
+void PrintNumber(char* label, int number)
+{
+  Serial.print(label);
+  Serial.print("=");
+  Serial.println(number, DEC);
+}
